f093a5070209951d9fe3c975eedc0375450b0684
Sanchez.246191 <Sanchez.246191@LAB-A-PC31>
2023-10-16 19:47:03 +0800

    Add project files.

---
+++ FrmBasicThread_Sanchez/BasicThread.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace FrmBasicThread_Sanchez
+{
+    partial class BasicThread
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBefore = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblBefore
+            // 
+            this.lblBefore.AutoSize = true;
+            this.lblBefore.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBefore.Location = new System.Drawing.Point(78, 39);
+            this.lblBefore.Name = "lblBefore";
+            this.lblBefore.Size = new System.Drawing.Size(277, 32);
+            this.lblBefore.TabIndex = 0;
+            this.lblBefore.Text = "-Before starting thread-";
+            // 
+            // btnRun
+            // 
+            this.btnRun.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnRun.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRun.Font = new System.Drawing.Font("Sans Serif Collection", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRun.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnRun.Location = new System.Drawing.Point(165, 147);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(110, 35);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = false;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // BasicThread
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(438, 219);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lblBefore);
+            this.Name = "BasicThread";
+            this.Text = "BasicThread";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBefore;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ FrmBasicThread_Sanchez/BasicThread.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmBasicThread_Sanchez
+{
+    public partial class BasicThread : Form
+    {
+        public BasicThread()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            ThreadStart delStart = new ThreadStart(MyThreadClass.Thread1);
+            Thread ThreadA = new Thread(MyThreadClass.Thread1);
+            ThreadA.Name = "Thread A Process";
+            Thread ThreadB = new Thread(MyThreadClass.Thread1);
+            ThreadB.Name = "Thread B Process";
+            Console.WriteLine(lblBefore.Text.ToString());
+            ThreadA.Start();
+            ThreadB.Start();
+            ThreadA.Join();
+            ThreadB.Join();
+            lblBefore.Text = "-End of Thread-";
+            Console.WriteLine(lblBefore.Text.ToString());
+        }
+    }
+}
